e11fba6ab61e382b467263e813e461ed9640f205
Sapo <v32031491@gmail.com>
2026-06-28 00:44:21 -0300

    Rename variable 'aaa' to 'escolha' for clarity

--- PA_e_PG.py
+++ PA_e_PG.py
@@ -1,15 +1,15 @@
-aaa = input('PA ou PG? ').lower()
+escolha = input('PA ou PG? ').lower()
 numero = int(input('Escolha o primeiro número: '))
 termo = int(input('Qual posição você quer? '))
 razao = int(input('Qual é a razão? '))
 contador = 0
-if aaa == "pa":
+if escolha == "pa":
     while contador < termo + 1:
         resultado_pa = numero + (contador - 1) * razao
         print(resultado_pa)
         contador += 1
 
-elif aaa == "pg":
+elif escolha == "pg":
     while contador < termo + 1:
         resultado_pg = numero * razao ** (contador - 1)
         print(resultado_pg)
